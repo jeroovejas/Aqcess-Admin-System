41466d2ee787bdb25da2304355ab048d0e83dba6
dzonetechnologies23@gmail.com <153366581+dzonetechnologies@users.noreply.github.com>
2024-10-16 19:24:05 +0500

    updated

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,36 +1,40 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import { MailerSend, EmailParams, Sender, Recipient } from 'mailersend';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request) {
+    if (req.method === 'GET') {
+        // Process a POST request
+        try {
+            // Initialize MailerSend with your API key from .env
+            const mailerSend = new MailerSend({
+                apiKey: process.env.MAIL_SENDER_API_KEY as string, // Ensure your API key is set in the .env file
+            });
 
-    try {
-        // Initialize MailerSend with your API key from .env
-        const mailerSend = new MailerSend({
-            apiKey: process.env.NEXT_PUBLIC_MAIL_SENDER_API_KEY as string, // Ensure your API key is set in the .env file
-        });
+            // Sender information
+            const sentFrom = new Sender("jeovejas@gmail.com", "Muhammad Faiz Rasool");
 
-        // Sender information
-        const sentFrom = new Sender("jeovejas@gmail.com", "Muhammad Faiz Rasool");
+            const recipients = [
+                new Recipient("emp.metasolutionspk@gmail.com", "Meta Solutions")
+            ];
 
-        const recipients = [
-            new Recipient("emp.metasolutionspk@gmail.com", "Meta Solutions")
-        ];
+            // Create email parameters
+            const emailParams = new EmailParams()
+                .setFrom(sentFrom)
+                .setTo(recipients)
+                // .setReplyTo(sentFrom)
+                .setSubject("This is a Subject")
+                .setHtml("<strong>This is the HTML content</strong>")
+                .setText("This is the text content");
+            // Send the email
+            await mailerSend.email.send(emailParams);
 
-        // Create email parameters
-        const emailParams = new EmailParams()
-            .setFrom(sentFrom)
-            .setTo(recipients)
-            // .setReplyTo(sentFrom)
-            .setSubject("This is a Subject")
-            .setHtml("<strong>This is the HTML content</strong>")
-            .setText("This is the text content");
-        // Send the email
-        await mailerSend.email.send(emailParams);
-
-        // Send a success response
-        return res.status(200).json({ message: 'Email sent successfully' });
-    } catch (error) {
-        console.error('Error sending email:', error);
-        return res.status(500).json({ message: 'Failed to send email', error });
+            // Send a success response
+            return NextResponse.json({ message: 'Email sent successfully' });
+        } catch (error) {
+            console.error('Error sending email:', error);
+            return NextResponse.json({ message: 'Failed to send email', error });
+        }
+    } else {
+        return NextResponse.json({ message: 'method not applicable' });
     }
 }
