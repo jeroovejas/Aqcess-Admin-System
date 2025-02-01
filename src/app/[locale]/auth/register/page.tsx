37a9dd51bdcbf32ab4ff251fac7ecbcd63ac4bab
adeel98amir@gmail.com <adeel98amir@gmail.com>
2025-02-01 18:17:36 +0500

    fix Sign up bug in CRM

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -227,10 +227,10 @@ const Register: React.FC = () => {
                     <div className="relative">
                       <input
                         type="text"
-                        name={t('SIGNUP.lastName')}
+                        name="last_name"
                         value={formState.last_name}
                         onChange={handleInputChange}
-                        placeholder="Last name"
+                        placeholder={t('SIGNUP.lastName')}
                         className="w-full rounded-lg border border-stroke bg-transparent py-2 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         required
 
