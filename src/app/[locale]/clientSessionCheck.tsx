b7712487209f9ccd92f7aa3fef7c4d8038541eb5
adeel98amir@gmail.com <adeel98amir@gmail.com>
2025-04-05 18:04:34 +0500

    fix issues in accounting CRM

--- src/app/[locale]/clientSessionCheck.tsx
+++ src/app/[locale]/clientSessionCheck.tsx
@@ -4,8 +4,8 @@ import { clearToken } from "@/store/Slices/AuthSlice";
 import { useAppDispatch } from "@/store/hooks";
 import { useRouter } from '@/navigation';
 
-// const AUTO_SIGNOUT_TIME = 1 * 60 * 1000; // 2 minutes in milliseconds For Test Purpose
-const AUTO_SIGNOUT_TIME = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+const AUTO_SIGNOUT_TIME = 1 * 60 * 1000; // 2 minutes in milliseconds For Test Purpose
+// const AUTO_SIGNOUT_TIME = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
 
 
 const ClientSessionCheck = () => {
